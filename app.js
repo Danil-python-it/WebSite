4545372fb1631bc4a539d5f7aea708ac7d494533
Danil Advakatov-volkov <81391324+Danil-python-it@users.noreply.github.com>
2023-09-18 09:18:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,26 +5,35 @@ var width = window.getComputedStyle(ctn_images).width;
 var max_position = parseInt(width.charAt(0) + width.charAt(1) + width.charAt(2) + width.charAt(3));
 var dist = max_position/5;
 var x_start = null;
+var x_move = null;
 
-function touchstart(event){
-    x_start = event.clientX
-    console.log(event.clientX);
-}
-function touchmove(event){
 
-}
-function touchend(event){
-    x_end = event.clientX
-    if(x_end < x_start){
+
+
+document.getElementById("iii").addEventListener("mousedown", function(event){
+    x_start = event.clientX;
+    console.log(x_start, "--");
+}, true);
+
+document.getElementById("iii").addEventListener("mousemove", function(event){
+    x_move = event.clientX;
+    console.log(x_end, "//");
+}, false);
+document.getElementById("iii").addEventListener("mouseup", function(){
+    console.log(x_move, x_start);
+    if(x_move < x_start){
         console.log("left");
         move_left()
     }
-    else if(x_end > x_start){
+    else if(x_move > x_start){
         console.log("right");
         move_right()
     }
-    
-}
+}, false);
+
+
+
+
 
 function SetIndicator(){
     let indicators = document.querySelectorAll(".container_indicator");
@@ -55,7 +64,4 @@ function move_right(){
     SetIndicator();
     ctn_images.style.left = position.toString()+"px";
 }
-
-
-
 let timerID = setInterval(() => move_right(), 10000);
